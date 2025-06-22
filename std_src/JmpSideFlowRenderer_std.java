53d501fc06396d037d40a3f5aa1fef123aadafa3
Suka-T <absolute6626x@gmail.com>
2025-06-22 22:08:40 +0900

    プラグインの呼び出し方法を修正。

--- std_src/JmpSideFlowRenderer_std.java
+++ std_src/JmpSideFlowRenderer_std.java
@@ -1,14 +1,8 @@
-import jmp.JMPLoader;
 import plg.JmpSideFlowRenderer;
 import std.StandAlonePluginInvoker;
 
 public class JmpSideFlowRenderer_std {
     public static void main(String[] args) {
-        JMPLoader.UsePluginDirectory = false;
-        JMPLoader.UseConfigFile = false;
-        JMPLoader.UseHistoryFile = false;
-        JMPLoader.UseSkinFile = false;
-
-        StandAlonePluginInvoker.exec(args, new JmpSideFlowRenderer());
+        StandAlonePluginInvoker.execSimple(args, new JmpSideFlowRenderer());
     }
 }
